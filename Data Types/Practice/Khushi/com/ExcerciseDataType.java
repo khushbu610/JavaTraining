cf85184216f752ab05c56f42bdb37b99d42569b3
khushbu610 <57229586+khushbu610@users.noreply.github.com>
2019-10-31 16:01:21 -0400

    Update ExcerciseDataType.java

--- Data Types/Practice/Khushi/com/ExcerciseDataType.java
+++ Data Types/Practice/Khushi/com/ExcerciseDataType.java
@@ -14,6 +14,21 @@ public class ExcerciseDataType {
 	public static void main( String[] args ){  
  
 		//TODO you need to create your logic here..
+		String myName ="Khushbu";
+		int subject1 =80;
+		int subject2 = 30;
+		int average = (subject1 + subject2)/2;
+		boolean a = true;
+		boolean b = false;
+		
+	System.out.println(myName);
+	System.out.println(average);
+	System.out.println("Total marks you obtained out of 200 is:" + (subject1 + subject2));
+	System.out.println("Average of two subject is: " + ((subject1 + subject2)/2));
+	System.out.println("a && b = " + (a && a));
+	System.out.println("a || b = " + (a||a) );
+	System.out.println("!(a && b) = " + !(a && a));	
+			
 		
 
 	}
